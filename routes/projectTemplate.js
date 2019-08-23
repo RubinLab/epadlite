c6d6d11e4bb6936bbf001a469161024d604b9329
Emel Alkim <emelalkim@msn.com>
2019-08-22 20:05:56 -0700

    adds project template routes

---
+++ routes/projectTemplate.js
@@ -0,0 +1,107 @@
+// defines routes for templates
+async function routes(fastify) {
+  fastify.route({
+    method: 'POST',
+    url: '/projects/:projectId/templates',
+    schema: {
+      params: {
+        type: 'object',
+        properties: {
+          projectId: {
+            type: 'string',
+          },
+        },
+      },
+    },
+    handler: fastify.saveTemplateToProject,
+  });
+
+  fastify.route({
+    method: 'PUT',
+    url: '/projects/:projectId/templates/:uid',
+    schema: {
+      params: {
+        type: 'object',
+        properties: {
+          projectId: {
+            type: 'string',
+          },
+          uid: {
+            type: 'string',
+          },
+        },
+      },
+    },
+    handler: fastify.saveTemplateToProject,
+  });
+
+  // fastify.route({
+  //   method: 'DELETE',
+  //   url: '/projects/:projectId/templates/:uid',
+  //   schema: {
+  //     params: {
+  //       type: 'object',
+  //       properties: {
+  //         projectId: {
+  //           type: 'string',
+  //         },
+  //         uid: {
+  //           type: 'string',
+  //         },
+  //       },
+  //     },
+  //   },
+  //   handler: fastify.deleteTemplate,
+  // });
+
+  // GET {s}/templates
+  fastify.route({
+    method: 'GET',
+    url: '/projects/:projectId/templates',
+    querystring: {
+      format: { type: 'string' },
+    },
+    schema: {
+      params: {
+        type: 'object',
+        properties: {
+          projectId: {
+            type: 'string',
+          },
+        },
+      },
+    },
+    // schema: {
+    //   response: {
+    //     200: 'templates_schema#',
+    //   },
+    // },
+
+    handler: fastify.getProjectTemplates,
+  });
+
+  // fastify.route({
+  //   method: 'POST',
+  //   url: '/projects/:projectId/templates/download',
+  //   schema: {
+  //     body: {
+  //       type: 'array',
+  //       items: {
+  //         type: 'string',
+  //       },
+  //     },
+  //     params: {
+  //       type: 'object',
+  //       properties: {
+  //         projectId: {
+  //           type: 'string',
+  //         },
+  //       },
+  //     },
+  //   },
+
+  //   handler: fastify.getTemplatesFromUIDs,
+  // });
+}
+
+module.exports = routes;
