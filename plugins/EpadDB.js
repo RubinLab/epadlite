992a18a6a2945e43edd35c0ee15b228fea850d0c
Emel Alkim <emelalkim@msn.com>
2019-08-22 20:01:06 -0700

    adds saveTemplateToProject and getProjectTemplates methods to epaddb plugin and syncs the db with models on each init (only creates tables that are not already created and imported in code)

--- plugins/EpadDB.js
+++ plugins/EpadDB.js
@@ -2,11 +2,14 @@ const fp = require('fastify-plugin');
 
 async function epaddb(fastify) {
   const Project = fastify.orm.import(`${__dirname}/../models/project`);
+  const ProjectTemplate = fastify.orm.import(`${__dirname}/../models/project_template`);
+
   fastify.decorate('initMariaDB', async () => {
     // Test connection
     fastify.orm
       .authenticate()
-      .then(() => {
+      .then(async () => {
+        await fastify.orm.sync();
         console.log('Connection to mariadb has been established successfully.');
       })
       .catch(err => {
@@ -45,6 +48,58 @@ async function epaddb(fastify) {
         reply.code(503).send(err.message);
       });
   });
+
+  fastify.decorate('saveTemplateToProject', async (request, reply) => {
+    try {
+      let templateUid = request.params.uid;
+      if (request.body) {
+        await fastify.saveTemplateInternal(request.body);
+        templateUid = request.body.TemplateContainer.uid;
+      }
+      const project = await Project.findOne({ where: { projectid: request.params.projectId } });
+
+      await ProjectTemplate.create({
+        project_id: project.id,
+        template_uid: templateUid,
+        enabled: true,
+        creator: request.query.username,
+        updatetime: Date.now(),
+      });
+      reply.code(200).send('Saving successful');
+    } catch (err) {
+      // TODO Proper error reporting implementation required
+      console.log(`Error in save: ${err}`);
+      reply.code(503).send(`Saving error: ${err}`);
+    }
+  });
+
+  fastify.decorate('getProjectTemplates', async (request, reply) => {
+    try {
+      const project = await Project.findOne({ where: { projectid: request.params.projectId } });
+      const templateUids = [];
+      ProjectTemplate.findAll({ where: { project_id: project.id } }).then(projectTemplates => {
+        // projects will be an array of Project instances with the specified name
+        projectTemplates.forEach(projectTemplate =>
+          templateUids.push(projectTemplate.template_uid)
+        );
+        console.log(templateUids);
+        fastify
+          .getTemplatesFromUIDsInternal(request.query, templateUids)
+          .then(result => {
+            if (request.query.format === 'stream') {
+              reply.header('Content-Disposition', `attachment; filename=templates.zip`);
+            }
+            reply.code(200).send(result);
+          })
+          .catch(err => reply.code(503).send(err));
+      });
+    } catch (err) {
+      // TODO Proper error reporting implementation required
+      console.log(`Error in save: ${err}`);
+      reply.code(503).send(`Saving error: ${err}`);
+    }
+  });
+
   fastify.after(async () => {
     try {
       await fastify.initMariaDB();
