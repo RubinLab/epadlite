75bdc9bd44d4b226eba0689bb2fbf5758cc47b95
Emel Alkim <emelalkim@msn.com>
2019-08-27 20:09:11 -0700

    modifies route loading to start handling lite and thick at the same time

--- server.js
+++ server.js
@@ -76,18 +76,21 @@ if (config.mode === 'thick') fastify.register(require('./plugins/EpadDB'));
 // this should be done after CouchDB plugin to be able to use the accessor methods
 // for both thick and lite
 fastify.register(require('./routes/template')); // eslint-disable-line global-require
+// adding generic routes for completion, in lite, they work the same as the projects/lite prefix
+fastify.register(require('./routes/aim')); // eslint-disable-line global-require
+fastify.register(require('./routes/dicomweb')); // eslint-disable-line global-require
+fastify.register(require('./routes/other')); // eslint-disable-line global-require
+
 fastify.register(require('./routes/worklist')); // eslint-disable-line global-require
 
-// if in lite mode
-if (config.mode === 'lite') {
-  fastify.register(require('./routes/aim'), { prefix: '/projects/lite' }); // eslint-disable-line global-require
-  fastify.register(require('./routes/dicomweb'), { prefix: '/projects/lite' }); // eslint-disable-line global-require
-  fastify.register(require('./routes/other'), { prefix: '/projects/lite' }); // eslint-disable-line global-require
-} else if (config.mode === 'thick') {
+if (config.mode === 'thick') {
   fastify.register(require('./routes/project')); // eslint-disable-line global-require
   fastify.register(require('./routes/projectTemplate')); // eslint-disable-line global-require
-  fastify.register(require('./routes/other')); // eslint-disable-line global-require
+  // fastify.register(require('./routes/projectAim')); // eslint-disable-line global-require
+  // fastify.register(require('./routes/projectDicomweb')); // eslint-disable-line global-require
+  // fastify.register(require('./routes/projectOther')); // eslint-disable-line global-require
 }
+// }
 // authCheck routine checks if there is a bearer token or encoded basic authentication
 // info in the authorization header and does the authentication or verification of token
 // in keycloak
