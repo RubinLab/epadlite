d897eb2d81938c925392c7e542f135565dce1cfd
Emel Alkim <emelalkim@msn.com>
2020-04-03 00:02:12 -0700

    removes keep alive

--- plugins/Other.js
+++ plugins/Other.js
@@ -727,7 +727,6 @@ async function other(fastify) {
   fastify.decorate('getNotifications', (request, reply) => {
     try {
       reply.res.writeHead(200, {
-        Connection: 'keep-alive',
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
         'Access-Control-Allow-Origin': '*',
