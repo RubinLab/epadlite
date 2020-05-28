55f6e04317d8ad0c639faa07fd5850962c74a726
Emel Alkim <emelalkim@msn.com>
2020-05-28 14:47:51 -0700

    stop giving the upload notification for segmentation save

--- plugins/Other.js
+++ plugins/Other.js
@@ -105,9 +105,10 @@ async function other(fastify) {
                 } else if (config.env === 'test') reply.code(200).send();
                 else {
                   fastify.log.info(`Upload Completed ${filenames}`);
-                  new EpadNotification(request, 'Upload Completed', filenames, true).notify(
-                    fastify
-                  );
+                  if (!(filenames.length === 1 && filenames[0] === 'blob.dcm'))
+                    new EpadNotification(request, 'Upload Completed', filenames, true).notify(
+                      fastify
+                    );
                 }
               } else if (config.env === 'test') {
                 reply.send(
