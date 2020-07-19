5a52c9a23c622e6258fff347eff4a5cae3c2e58e
cavit <cavitaltindag@gmail.com>
2020-07-19 15:41:22 -0700

    csv extension allowded on upload. federated learning plugin needs this file

--- config/index.js
+++ config/index.js
@@ -34,7 +34,7 @@ config.dicomWebConfig.baseUrl =
   process.env.DICOMWEB_BASEURL || config.dicomWebConfig.baseUrl || 'http://hostname';
 
 config.mode = process.env.MODE || config.mode || 'lite'; // default lite
-config.imageExt = process.env.IMAGE_EXT || config.imageExt || 'jpg|jpeg|png';
+config.imageExt = process.env.IMAGE_EXT || config.imageExt || 'jpg|jpeg|png|csv';
 config.reportExt = process.env.REPORT_EXT || config.reportExt || 'txt|pdf';
 config.validExt = `${config.imageExt}|${config.reportExt}`;
 config.prefix = process.env.PREFIX || config.prefix || '';
