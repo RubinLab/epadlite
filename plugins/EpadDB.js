b3101076dd67d980a4c499e70afaa2129efb0f6b
Emel Alkim <emelalkim@msn.com>
2020-09-21 21:46:56 -0700

    adds a query param longitudinal_ref. when it is set to true, the aims are grouped by tracking unique identifier (if present for all aims) or name and a map containing type and a reference aim is returned (aim from the latest study)

--- plugins/EpadDB.js
+++ plugins/EpadDB.js
@@ -4549,6 +4549,88 @@ async function epaddb(fastify, options, done) {
         reply.header('Content-Disposition', `attachment; filename=annotations.zip`);
       } else if (request.query.format === 'summary') {
         result = result.map(obj => ({ ...obj, projectID: request.params.project }));
+      } else if (request.query.longitudinal_ref) {
+        const aimsByName = {};
+        const aimsByTUID = {};
+        let tUIDCount = 0;
+        result.forEach(aim => {
+          const name = aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0].name.value.split(
+            '~'
+          )[0];
+          const studyDate =
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .imageReferenceEntityCollection.ImageReferenceEntity[0].imageStudy.startDate.value;
+          let type;
+          // recist
+          if (
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .imagingObservationEntityCollection &&
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .imagingObservationEntityCollection.ImagingObservationEntity[0]
+              .imagingObservationCharacteristicCollection &&
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0].imagingObservationEntityCollection.ImagingObservationEntity[0].imagingObservationCharacteristicCollection.ImagingObservationCharacteristic[0].label.value.toLowerCase() ===
+              'type'
+          )
+            type =
+              aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+                .imagingObservationEntityCollection.ImagingObservationEntity[0]
+                .imagingObservationCharacteristicCollection.ImagingObservationCharacteristic[0]
+                .typeCode[0]['iso:displayName'].value;
+          // recist v2
+          if (
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .imagingObservationEntityCollection &&
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .imagingObservationEntityCollection.ImagingObservationEntity[0]
+              .imagingObservationCharacteristicCollection &&
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .imagingObservationEntityCollection.ImagingObservationEntity[0]
+              .imagingObservationCharacteristicCollection.ImagingObservationCharacteristic[1] &&
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0].imagingObservationEntityCollection.ImagingObservationEntity[0].imagingObservationCharacteristicCollection.ImagingObservationCharacteristic[1].label.value.toLowerCase() ===
+              'type'
+          )
+            type =
+              aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+                .imagingObservationEntityCollection.ImagingObservationEntity[0]
+                .imagingObservationCharacteristicCollection.ImagingObservationCharacteristic[1]
+                .typeCode[0]['iso:displayName'].value;
+
+          if (name && !aimsByName[name]) aimsByName[name] = { aim, type };
+          else if (
+            aimsByName[name].aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .imageReferenceEntityCollection.ImageReferenceEntity[0].imageStudy.startDate.value <
+            studyDate
+          )
+            aimsByName[name] = { aim, type };
+          if (
+            aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+              .trackingUniqueIdentifier
+          ) {
+            if (
+              !aimsByTUID[
+                aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+                  .trackingUniqueIdentifier.root
+              ]
+            )
+              aimsByTUID[
+                aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0].trackingUniqueIdentifier.root
+              ] = { aim, type };
+            else if (
+              aimsByTUID[
+                aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
+                  .trackingUniqueIdentifier.root
+              ].aim.ImageAnnotationCollection.imaÎgeAnnotations.ImageAnnotation[0]
+                .imageReferenceEntityCollection.ImageReferenceEntity[0].imageStudy.startDate.value <
+              studyDate
+            )
+              aimsByTUID[
+                aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0].trackingUniqueIdentifier.root
+              ] = { aim, type };
+            tUIDCount += 1;
+          }
+        });
+        if (tUIDCount === result.length) result = aimsByTUID;
+        else result = aimsByName;
       }
       reply.code(200).send(result);
     } catch (err) {
