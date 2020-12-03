ceb012fe51ac8fc938fc5a23b15b0657f57eeb0a
Emel Alkim <emelalkim@msn.com>
2020-12-03 19:08:06 +0300

    fixes the content type parser for fastify 3

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const fastify = require('fastify')({
       : '',
 });
 
-fastify.addContentTypeParser('*', (req, done) => {
+fastify.addContentTypeParser('*', (_, payload, done) => {
   let data = [];
-  req.on('data', chunk => {
+  payload.on('data', chunk => {
     data.push(chunk);
   });
-  req.on('end', () => {
+  payload.on('end', () => {
     data = Buffer.concat(data);
     done(null, data);
   });
