719d8f006982fe4cdde1b751ebc63c66727935ec
Emel Alkim <emelalkim@msn.com>
2021-03-23 21:28:43 -0700

    add bidirectional test

--- test/aimConvertTest.js
+++ test/aimConvertTest.js
@@ -7,7 +7,7 @@ const { expect } = chai;
 
 describe.only('Aim Convert Tests', () => {
   it('should convert aim to dicomsr ', (done) => {
-    const jsonBuffer = JSON.parse(fs.readFileSync('test/data/roi_sample_aim.json'));
+    const jsonBuffer = JSON.parse(fs.readFileSync('test/data/bidirectional_recist.json'));
     chai
       .request(`http://${process.env.host}:${process.env.port}`)
       .put(`/dicomsr`)
