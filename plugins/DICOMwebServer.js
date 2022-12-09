9ebf4ea4ddf5734ada7a59bf2992a9b5583351ec
Emel ALKIM <emelalkim@gmail.com>
2022-12-08 16:14:30 -0800

    fix format

--- plugins/DICOMwebServer.js
+++ plugins/DICOMwebServer.js
@@ -1001,7 +1001,9 @@ async function dicomwebserver(fastify) {
                     : params.study,
                 seriesUID: value['0020000E'].Value[0],
                 seriesDate:
-                  value['00080021'] && value['00080021'].Value && value['00080021'].Value[0] ? value['00080021'].Value[0] : '',
+                  value['00080021'] && value['00080021'].Value && value['00080021'].Value[0]
+                    ? value['00080021'].Value[0]
+                    : '',
                 seriesDescription:
                   value['0008103E'] && value['0008103E'].Value && value['0008103E'].Value[0]
                     ? value['0008103E'].Value[0]
