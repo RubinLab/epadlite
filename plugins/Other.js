cad17d690b4c8ab99bded58236661f444338a455
Emel ALKIM <emelalkim@gmail.com>
2023-09-20 14:54:57 -0700

    Revert "add httpslink override"
    This reverts commit fe5fa627d038a9ac915e0a52a8fd0ef893458fd5.

--- plugins/Other.js
+++ plugins/Other.js
@@ -730,9 +730,7 @@ async function other(fastify) {
             else {
               fastify.log.info(`Zip file ready in ${result}`);
               // get the protocol and hostname from the request
-              const link = `${config.httpsLink ? 'https' : request.protocol}://${
-                request.hostname
-              }${result}`;
+              const link = `${request.protocol}://${request.hostname}${result}`;
               fastify.log.info(`LINK TO DOWNLOAD ZIP ${link}`);
               // send notification and/or email with link
               if (request)
