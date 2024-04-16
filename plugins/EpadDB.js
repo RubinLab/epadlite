a9037f9db19adc72c7185dd407c22d02b1f003f7
Emel ALKIM <emelalkim@gmail.com>
2024-04-16 02:19:28 -0700

    allow empty year

--- plugins/EpadDB.js
+++ plugins/EpadDB.js
@@ -13385,7 +13385,6 @@ async function epaddb(fastify, options, done) {
   fastify.decorate('getMonthlyTeachingStats', async (request, reply) => {
     // eslint-disable-next-line prefer-const
     let { year } = request.query;
-    if (!year) year = new Date().getFullYear();
     let yearFilter;
     if (year) yearFilter = ` where year = '${year}'`;
 
