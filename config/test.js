64d2bba816fc0b5b06b1d19172a35239ee9f36ce
Emel ALKIM <emelalkim@gmail.com>
2024-05-22 14:29:49 -0700

    edit teaching project and test mode

--- config/test.js
+++ config/test.js
@@ -8,7 +8,7 @@ module.exports = {
   logger: {
     level: 'error',
   },
-  mode: 'thick',
+  mode: 'teaching',
   thickDb: {
     name: 'test_epaddb',
     host: 'localhost',
@@ -21,5 +21,5 @@ module.exports = {
   pollDW: 0,
   auditLog: true,
   versionAudit: true,
-  teachingProject: 'testaim3',
+  teachingProject: 'testaim',
 };
