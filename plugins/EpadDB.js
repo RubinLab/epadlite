0c7e4fdea2cec578c039c8b92edb60ef0ed0b70a
Emel ALKIM <emelalkim@gmail.com>
2024-09-29 23:01:11 -0700

    remove lower

--- plugins/EpadDB.js
+++ plugins/EpadDB.js
@@ -13681,11 +13681,7 @@ async function epaddb(fastify, options, done) {
             updatetime: Date.now(),
           },
           {
-            host: Sequelize.where(
-              Sequelize.fn('LOWER', Sequelize.col('host')),
-              'LIKE',
-              `%${host.split('|')[1]}%`
-            ),
+            host: Sequelize.where(Sequelize.col('host'), 'LIKE', `%${host.split('|')[1]}%`),
             template_code: userTFStats[i].template || config.teachingTemplate,
             user_id: userTFStats[i].userId,
             year,
